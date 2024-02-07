8c8b46711d687f835d33d0af7098a7dbd7640a1f
bashair0 <bisho5.ba@gmail.com>
2024-02-07 20:26:01 +0800

    Add and style the calculator screen and buttons

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const customRadio = document.querySelectorAll('.custom-radio')
 const inputs = document.querySelectorAll('input[name="theme"]')
+const buttons = document.querySelectorAll('button')
+const result = document.querySelector('.result')
 
-inputs.forEach(input => {
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    result.innerText += button.value
+  })
+})
+
+/* inputs.forEach(input => {
   input.addEventListener('click', () => {
     if (input.checked === true) {
       console.log(input)
     }
   })
 })
+ */
