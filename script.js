6e0bb19764d9efd799d63d0d73d10efda6c9f0c4
bashair0 <bisho5.ba@gmail.com>
2024-02-19 18:47:33 +0800

    Display the clicked number on screen and make the reset button work

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
-const customRadio = document.querySelectorAll('.custom-radio')
-const inputs = document.querySelectorAll('input[name="theme"]')
-const buttons = document.querySelectorAll('button')
-const result = document.querySelector('.result')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equal]')
+const deleteButton = document.querySelector('[data-delete]')
+const resetButton = document.querySelector('[data-reset]')
+const previousOperandTextElement = document.querySelector(
+  '[data-previous-operand]'
+)
+const currentOperandTextElement = document.querySelector(
+  '[data-current-operand]'
+)
 
-buttons.forEach(button => {
+operator = ''
+previousOperand = ''
+currentOperand = ''
+
+/* ********** */
+/* FUNCTIONS */
+/* ********** */
+
+let add = (x, y) => {
+  return x + y
+}
+let sub = (x, y) => {
+  return x - y
+}
+let multi = (x, y) => {
+  return x * y
+}
+let div = (x, y) => {
+  if (y === 0) {
+    return 'Error! Division by zero.'
+  } else {
+    return x / y
+  }
+}
+
+function operate (operator, x, y) {
+  switch (operator) {
+    case '+':
+      return add(x, y)
+    case '-':
+      return sub(x, y)
+    case '×':
+      return multi(x, y)
+    case '÷':
+      return div(x, y)
+    default:
+      return 'Invalid operator'
+  }
+}
+
+numberButtons.forEach(button => {
   button.addEventListener('click', () => {
-    result.innerText += button.value
+    appendNumber(button.innerText)
+    currentOperandTextElement.textContent = currentOperand
   })
 })
 
-/* inputs.forEach(input => {
-  input.addEventListener('click', () => {
-    if (input.checked === true) {
-      console.log(input)
-    }
-  })
+resetButton.addEventListener('click', () => {
+  resetCalculator()
+  currentOperandTextElement.textContent = currentOperand
+  previousOperandTextElement.textContent = currentOperand
 })
- */
+
+function resetCalculator () {
+  currentOperand = ''
+  previousOperand = ''
+  operator = ''
+}
+
+function appendNumber (number) {
+  if (number === '.' && currentOperand.includes('.')) return
+  if (currentOperand.length <= 7) {
+    currentOperand = currentOperand.toString() + number.toString()
+  }
+}
