8647c62c5aac035703b4f9758e8d1502f31de877
bashair0 <bisho5.ba@gmail.com>
2024-02-24 22:35:27 +0800

    Add update display function and make all of the other buttons work properly

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const currentOperandTextElement = document.querySelector(
   '[data-current-operand]'
 )
 
-operator = ''
-previousOperand = ''
-currentOperand = ''
+let operator = ''
+let previousOperand = ''
+let currentOperand = ''
 
 /* ********** */
 /* FUNCTIONS */
@@ -29,13 +29,15 @@ let multi = (x, y) => {
 }
 let div = (x, y) => {
   if (y === 0) {
-    return 'Error! Division by zero.'
+    return '∞'
   } else {
     return x / y
   }
 }
 
-function operate (operator, x, y) {
+function operate () {
+  let x = parseFloat(previousOperand)
+  let y = parseFloat(currentOperand)
   switch (operator) {
     case '+':
       return add(x, y)
@@ -53,15 +55,44 @@ function operate (operator, x, y) {
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
     appendNumber(button.innerText)
-    currentOperandTextElement.textContent = currentOperand
+    updateDisplay()
   })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    handleOperator(button.innerText)
+    updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', () => {
+  getResult()
+  updateDisplay()
+  previousOperandTextElement.textContent = previousOperand
+  currentOperandTextElement.textContent = currentOperand
 })
 
 resetButton.addEventListener('click', () => {
   resetCalculator()
-  currentOperandTextElement.textContent = currentOperand
-  previousOperandTextElement.textContent = currentOperand
+  updateDisplay()
 })
+
+deleteButton.addEventListener('click', () => {
+  del()
+  updateDisplay()
+})
+
+function getResult () {
+  if (previousOperand === '' || currentOperand === '') return
+  currentOperand = operate(
+    operator,
+    parseFloat(previousOperand),
+    parseFloat(currentOperand)
+  )
+  previousOperand = ''
+  operator = ''
+}
 
 function resetCalculator () {
   currentOperand = ''
@@ -69,9 +100,58 @@ function resetCalculator () {
   operator = ''
 }
 
+function del () {
+  currentOperand = currentOperand.toString().slice(0, -1)
+}
+
 function appendNumber (number) {
   if (number === '.' && currentOperand.includes('.')) return
   if (currentOperand.length <= 7) {
     currentOperand = currentOperand.toString() + number.toString()
+  }
+}
+
+function getDisplayNumber (number) {
+  const stringNumber = number.toString()
+  const integerDigits = parseFloat(stringNumber.split('.')[0])
+  const decimalDigits = stringNumber.split('.')[1]
+  let integerDisplay
+  if (isNaN(integerDigits)) {
+    integerDisplay = ''
+  } else {
+    integerDisplay = integerDigits.toLocaleString('en', {
+      maximumFractionDigits: 0
+    })
+  }
+  if (decimalDigits != null) {
+    return `${integerDisplay}.${decimalDigits}`
+  } else {
+    return integerDisplay
+  }
+}
+
+function updateDisplay () {
+  currentOperandTextElement.innerText = getDisplayNumber(currentOperand)
+  if (operator != null) {
+    previousOperandTextElement.innerText = `${getDisplayNumber(
+      previousOperand
+    )} ${operator} `
+  } else {
+    previousOperandTextElement.innerText = ''
+  }
+}
+
+function handleOperator (op) {
+  if (currentOperand === '') return
+  if (previousOperand !== '') {
+    previousOperand = operate()
+    currentOperand = ''
+    operator = op
+    previousOperandTextElement.textContent = `${previousOperand} ${operator}`
+    currentOperandTextElement.textContent = currentOperand
+  } else {
+    operator = op
+    previousOperand = currentOperand
+    currentOperand = ''
   }
 }
